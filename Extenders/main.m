8520ee03fc05ddeaa3b51e1d45727bf6d0c8cfb7
Frizlab <fload@me.com>
2014-04-29 10:29:57 +0200

    Initial Commit

---
+++ Extenders/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Extenders
+//
+//  Created by François LAMBOLEY on 29/04/14.
+//  Copyright (c) 2014 FTW & Co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
